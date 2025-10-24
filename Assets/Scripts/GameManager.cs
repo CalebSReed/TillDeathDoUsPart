52da219f52059cd4698d5a1c9ddbd4b465108787
CalebSReed <csereed@gmail.com>
2025-10-23 23:37:57 -0700

    changing computers

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -102,6 +102,7 @@ public class GameManager : MonoBehaviour
         if (context.performed && endGame)
         {
             Application.Quit();
+            Debug.Log("Quit!");
         }
     }
 
